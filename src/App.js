1ef00759323da65b5c993c265f4056095f86f1a6
tcsajaychandra <54847507+tcsajaychandra@users.noreply.github.com>
2020-03-14 18:14:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,6 @@ import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-);
-    }
 
 class App extends Component {
   render() {
@@ -24,10 +16,11 @@ class App extends Component {
            <h3> Test React </h3> 
       
               </p>
-          
-  
-  setInterval(tick, 1000);
-          <a
+          <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+              <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
