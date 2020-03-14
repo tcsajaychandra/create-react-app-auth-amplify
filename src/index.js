93ee2d8123f8d86401fb2de61d0111a769af27f4
tcsajaychandra <54847507+tcsajaychandra@users.noreply.github.com>
2020-03-14 19:24:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import logo from './logo.svg';
-import './App.css';
 import * as serviceWorker from './serviceWorker';
 
-<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           <h3> Test React </h3> 
+ReactDOM.render(<App />, document.getElementById('root'));
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
-}
-
-setInterval(tick, 1000);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
