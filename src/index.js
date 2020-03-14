6017c04c157db01d0a5bde1b81a22c0dd073cd52
tcsajaychandra <54847507+tcsajaychandra@users.noreply.github.com>
2020-03-14 19:16:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import logo from './logo.svg';
+import './App.css';
 import * as serviceWorker from './serviceWorker';
+
+<div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+           <h3> Test React </h3> 
 
 function tick() {
   const element = (
     <div>
-      <h1>React Test </h1>
+      <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
     </div>
   );
@@ -15,7 +23,6 @@ function tick() {
 }
 
 setInterval(tick, 1000);
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
